c60ae02730e8669cee5ec992449dc8c40c3c12dd
wteo <87306585+wteo@users.noreply.github.com>
2022-02-04 08:32:11 +1100

    Update error.js

--- error.js
+++ error.js
@@ -1,13 +1,3 @@
-function check404Error (response) {
-    if (!response.ok) {
-        const errorMessage = `Error : ${response.status}`;
-        const h1 = document.createElement("h1");
-        h1.innerText = errorMessage;
-        body.appendChild(h1);
-    }
-    return response.json();
-}
-
 function catchError (error) {
     const h1 = document.createElement("h1");
     h1.innerText = error;
